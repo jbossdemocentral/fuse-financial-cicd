399cb6983c9ae409fe68860ff4ab0ff14fa81c90
gnunn1 <gerald.b.nunn@gmail.com>
2017-01-26 17:44:36 -0500

    Remove stuff before PR

--- baseTemplates.sh
+++
@@ -1,4 +0,0 @@
-oc login -u system:admin
-oc project openshift
-oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/application-templates-2.0.redhat-000026/fis-image-streams.json
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json
