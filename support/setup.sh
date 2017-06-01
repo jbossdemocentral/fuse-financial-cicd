21dcfdf0abfe93160d8b99aad84f42fa3e5f9f15
weimeilin79 <weimeilin@gmail.com>
2017-05-31 23:00:42 -0400

    update scripts and template to install

--- support/setup.sh
+++ support/setup.sh
@@ -1,17 +1,30 @@
+echo "Install the FIS 2.0 image stream"
+echo 
+
+#oc login -u system:admin
+#oc project openshift
+#oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/master/fis-image-streams.json -n openshift
+#oc import-image amq62-openshift --from=registry.access.redhat.com/jboss-amq-6/amq62-openshift -n openshift --confirm
+
+
 echo "Create fisdemo project to work in, and load the A-MQ 6.2 template"
 echo 
-#oc login -u developer
+
+
+oc login -u developer
 oc new-project fisdemo --display-name="Fuse Banking Demo - Dev and UAT" --description="Development and UAT environment for Agile Integration Banking Demo - Power by Red Hat Fuse"
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq62-basic.json
+
 
 echo "Setup the surrounding softwate and environment"
 echo
-echo "Start up JENKINS for pipeline...."
-oc new-app jenkins-ephemeral --param=JENKINS_PASSWORD=password
 echo "Start up MySQL for database access"
+oc create -f https://raw.githubusercontent.com/openshift/origin/master/examples/db-templates/mysql-ephemeral-template.json
 oc new-app --template=mysql-ephemeral --param=MYSQL_PASSWORD=password --param=MYSQL_USER=dbuser --param=MYSQL_DATABASE=sampledb
+
 echo "Start up Broker for bitcoin gateway"
-oc new-app --template=amq62-basic --param=MQ_USERNAME=admin --param=MQ_PASSWORD=admin
+oc import-image amq62-openshift --from=registry.access.redhat.com/jboss-amq-6/amq62-openshift --confirm
+oc create -f projecttemplates/amq62-openshift.json
+oc new-app --template=amq62-basic --param=MQ_USERNAME=admin --param=MQ_PASSWORD=admin --param=IMAGE_STREAM_NAMESPACE=fisdemo
 
 
 echo "Create Traditional Banking instance"
@@ -23,4 +36,4 @@ cd ../fisdemoblockchain/
 mvn fabric8:deploy
 
 cd ..
-oc process -f template-uat.yml | oc create -f -
+oc process -f support/projecttemplates/template-uat.yml | oc create -f -
--- support/setupcicd.sh
+++ support/setupcicd.sh
@@ -4,6 +4,9 @@ echo "This script prepare the CICD project for you"
 echo "Create a project to all pipelines"
 echo 
 
+echo "Create a project to all pipelines"
+echo 
+
 oc new-project fisdemocicd --display-name="Fuse Banking Pipeline" --description="All CI/CD Pipeline for Banking Demo"
 
 
@@ -13,8 +16,29 @@ echo
 oc policy add-role-to-group edit system:serviceaccounts:fisdemocicd -n fisdemo
 oc policy add-role-to-group edit system:serviceaccounts:fisdemocicd -n fisdemoprod
 
+echo "Start up JENKINS for pipeline...."
+oc new-app jenkins-ephemeral
+
 echo "Install all three pipelines"
 echo 
-oc create -f pipeline-uat.yml
-oc create -f pipeline-ab.yml
-oc create -f pipeline-allprod.yml
+oc create -f pipelinetemplates/pipeline-uat.yml
+oc create -f pipelinetemplates/pipeline-ab.yml
+oc create -f pipelinetemplates/pipeline-allprod.yml
+
+oc new-app pipeline-uat
+
+oc new-app pipeline-ab \
+--param=THREESCALE_URL=https://fusedemo-admin.3scale.net \
+--param=API_TOKEN=143d1a90ea5d369a88ec35310f06b86fec8569a1c34c21eaffc9f9906947962a \
+--param=APP_PLAN_ID=2357355899471 \
+--param=METRICS_ID=2555418019263 \
+--param=API_LIMITS=25 \
+--param=OPENSHIFT_HOST=192.168.64.2.nip.io
+
+oc new-app pipeline-allprod \
+--param=THREESCALE_URL=https://fusedemo-admin.3scale.net \
+--param=API_TOKEN=143d1a90ea5d369a88ec35310f06b86fec8569a1c34c21eaffc9f9906947962a \
+--param=APP_PLAN_ID=2357355899471 \
+--param=METRICS_ID=2555418019263 \
+--param=API_LIMITS=50 \
+--param=OPENSHIFT_HOST=192.168.64.2.nip.io
